7a65d5e4b2ff466ae9c3403ee0743d6d219bd383
ParkMiHyun <mihyunpark24@gmail.com>
2020-04-01 23:59:36 +0900

    서울에서 김서방 찾기

---
+++ src/lv1/findMrKim.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class findMrKim {
+    public String findKim(String[] seoul) {
+        int i= 0;
+        for(; i<seoul.length; i++)
+            if(seoul[i].equals("Kim")){
+                break;
+            }
+        StringBuffer str = new StringBuffer("김서방은 ");
+        str.append(i);
+        str.append(" 에 있다");
+        return answer = new String(str.toString());
+    }
+}
